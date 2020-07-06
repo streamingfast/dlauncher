daa7b8b7e4fcfa9ea082b41e6211f415f30e2b91
Julien Cassis <juliencassis@gmail.com>
2020-07-06 11:11:25 -0400

    fix autobind flags

--- flags/flags.go
+++ flags/flags.go
@@ -8,16 +8,17 @@ import (
 	"github.com/spf13/viper"
 )
 
-func AutoBind(root *cobra.Command, prefix string) {
+func AutoBind(root *cobra.Command, prefix string) map[string]bool {
 	viper.SetEnvPrefix(strings.ToUpper(prefix))
 	viper.AutomaticEnv()
 	replacer := strings.NewReplacer("-", "_")
 	viper.SetEnvKeyReplacer(replacer)
 
-	recurseCommands(root, nil)
+	return recurseCommands(root, nil)
 }
 
-func recurseCommands(root *cobra.Command, segments []string) {
+func recurseCommands(root *cobra.Command, segments []string) map[string]bool {
+	out := make(map[string]bool)
 	var segmentPrefix string
 	if len(segments) > 0 {
 		segmentPrefix = strings.Join(segments, "-") + "-"
@@ -25,14 +26,17 @@ func recurseCommands(root *cobra.Command, segments []string) {
 
 	root.PersistentFlags().VisitAll(func(f *pflag.Flag) {
 		newVar := segmentPrefix + "global-" + f.Name
+		out[newVar] = true
 		viper.BindPFlag(newVar, f)
 	})
 	root.Flags().VisitAll(func(f *pflag.Flag) {
 		newVar := f.Name
+		out[newVar] = true
 		viper.BindPFlag(newVar, f)
 	})
 
 	for _, cmd := range root.Commands() {
 		recurseCommands(cmd, append(segments, cmd.Name()))
 	}
+	return out
 }
