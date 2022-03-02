935fc12be373f6ae72c42356273ecf27cd2bf176
Matthieu Vachon <matt@streamingfast.io>
2022-03-02 10:41:56 -0500

    Removed unused

--- launcher/runtime.go
+++ launcher/runtime.go
@@ -4,12 +4,6 @@ import (
 	"github.com/streamingfast/bstream"
 )
 
-// TODO: TransformableBlock Interface is not placed in bstream yet, since we want
-// to ensure that we really narrowed down it's behavior, and naming convention
-type BlockTransformer interface {
-	TransformInPlace(blk *bstream.Block) error
-}
-
 type Runtime struct {
 	AbsDataDir string
 	Launcher   *Launcher
